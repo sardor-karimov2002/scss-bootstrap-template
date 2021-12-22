ed3047a163c620905241a59ee07520757df62374
sardor-karimov2002 <sardorbekkarimov155@gmail.com>
2021-12-22 23:10:37 +0500

    js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+alert("Saytimizga Xush kelibsiz😊")
+
+alert("Nimani qidirmoqdasz?")
+
+alert("Barcha malumotlar bizda")
+
+var result = prompt("3x4= qiymatini kiriting.")
+console.log("Javobi \n" + result)
+
+var result = prompt("Bu saytga kirishiz uchun maxfiylik xabar berilsin")
+if(result!=xa)alert("xabar berilmadi")
+console.log("javobi \n"+result)
